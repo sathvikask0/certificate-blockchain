42c8e66caed9f5ba0194bf7ab6d2a429a919dae0
Ethan Stykes <31500520+ethanstykes@users.noreply.github.com>
2018-12-28 22:28:00 +0530

    Add files via upload

--- blockchain.py
+++ blockchain.py
@@ -8,12 +8,12 @@ class Blockchain:
     def __init__(self):
         self.current_transactions = []
         self.chain = []
-        self.nodes = set()
-        self.leader = 0
+        self.nodes = {'192.168.0.5:5000':'address1','192.168.0.5:5001':'address2','192.168.0.5:5002':'address3','192.168.0.5:5003':'address4'}
+        self.leader = 1
         
         # Create the genesis block
         self.new_block(previous_hash='1a3f4561c2b32c1')
-
+    
     def register_node(self, address): # Make it static: Fixed nodes mapped to addresses(TASK)
         """
         Add a new node to the list of nodes
@@ -77,16 +77,27 @@ class Blockchain:
         :return: True if our chain was replaced, False if not
         """
 
-        neighbours = self.nodes
+        # Grab and verify the chains from all the nodes in our network in order
+        
+        neighbours = list((self.nodes).keys())
+        nodes_len = len(neighbours)
+        neighbours_new = []
+        
+        # Reconfiguring the order of nodes
+        for i in range(self.leader,nodes_len):
+            neighbours_new.append(neighbours[i])
+        for i in range(slef.leader):
+            neighbours_new.append(neighbours[i])
+        neighbours =neighbours_new
+        
         replaced = False
         
-        # We're only looking for chains longer than ours
-        max_length = len(self.chain)
-        
-        
-        
-        # Grab and verify the chains from all the nodes in our network in order (TASK)
+        i = self.leader
         for node in neighbours:
+            
+            # We're only looking for chains longer than ours
+            max_length = len(self.chain)
+            
             response = requests.get('http://{node}/chain')
 
             if response.status_code == 200:
@@ -97,6 +108,15 @@ class Blockchain:
                 if length > max_length and self.valid_chain(chain):
                     max_length = length
                     replaced = True
+                    
+                    # Update leader status
+                    self.leader = i
+        
+            # Counting leader position
+            if(i < nodes_len):
+                i+=1
+            else:
+                i=0
 
         # Replace our chain if we discovered a new, valid chain longer than ours
         if replaced:
